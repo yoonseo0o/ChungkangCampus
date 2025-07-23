e1e88355d565b548405d5f173ebecede581ab7eb
yoonseo0o <muddy0618@naver.com>
2025-07-24 06:29:47 +0900

    cameraMove

--- Assets/Script/CameraMove.cs
+++ Assets/Script/CameraMove.cs
@@ -1,19 +1,54 @@
+using System.Collections;
 using UnityEngine;
+using UnityEngine.UIElements;
 
 public class CameraMove : MonoBehaviour
 {
+    public enum DirectionState { Left, Right, None }
     [SerializeField] private float moveSpeed;
     [SerializeField] private Transform target;
-    [SerializeField] private float mouseXThreshold;
+    [SerializeField] private float targetOffsetX;
+    [SerializeField] private DirectionState currentState;
+    private Coroutine moveCo;
     // Start is called once before the first execution of Update after the MonoBehaviour is created
     void Start()
     {
-        
+         Player.OnPlayerMoved += Move;
+        currentState = DirectionState.None;
     }
 
     // Update is called once per frame
     void Update()
     {
+        Debug.DrawLine(transform.position-new Vector3(targetOffsetX, 0,0), transform.position + new Vector3(targetOffsetX, 0, 0),Color.green);
+    }
+    public void Move(Vector2 vec)
+    {
+        DirectionState state = vec.x>0? DirectionState.Left: DirectionState.Right;
+        if (currentState == state) return;
+        if (moveCo!=null) { StopCoroutine(moveCo); moveCo = null; }
 
+        currentState = state;
+        moveCo = StartCoroutine(FollowTarget());
+    }
+    private IEnumerator FollowTarget()
+    {
+        Debug.Log("called");
+        Vector3 targetVec = new Vector3(
+            currentState == DirectionState.Left ?
+            target.position.x + targetOffsetX : target.position.x - targetOffsetX,
+            target.position.y, transform.position.z);
+
+        while (true)
+        {
+            targetVec = new Vector3(
+                currentState == DirectionState.Left ? 
+                target.position.x + targetOffsetX : target.position.x - targetOffsetX,
+                target.position.y, transform.position.z);
+            transform.position = Vector3.MoveTowards(transform.position, targetVec, moveSpeed*Time.deltaTime); 
+            yield return null;
+        }
+        /*Debug.Log("목표 위치 도착 완료");
+        moveCo = null;*/
     }
 }
--- Assets/Script/Player.cs
+++ Assets/Script/Player.cs
@@ -4,21 +4,22 @@ using System.Xml.Serialization;
 using Unity.VisualScripting;
 using UnityEditor;
 using UnityEngine;
+using System;
 using UnityEngine.InputSystem;
 using UnityEngine.Rendering;
-enum MoveState { Idle, Walk, Run }
 public class Player : MonoBehaviour
 {
+    enum MoveState { Idle, Walk, Run }
     [Header("ResourceConfig")]
     float mana;
     [Header("StatusFlags")]
     bool isMask;
     [SerializeField] private MoveState moveState;
     [Header("ControlParameters")]
-    [SerializeField] private float mouseXThreshold;
+    [SerializeField] private float mouseXThreshold;//임계?
     [SerializeField] private float walkSpeed;
     [SerializeField] private float runSpeed;
-
+    public static event Action<Vector2> OnPlayerMoved; 
     Vector2 moveVec;
     Coroutine moveCo;
     // Start is called once before the first execution of Update after the MonoBehaviour is created
@@ -31,19 +32,25 @@ public class Player : MonoBehaviour
     void Update()
     {
         Move();
+        Debug.DrawLine(transform.position-new Vector3(mouseXThreshold,0,0), transform.position + new Vector3(mouseXThreshold, 0, 0),Color.red);
     } 
     private void Move()
     {
+        // 마우스 포인터가 플레이어의 어느쪽에 있는지 판단
         float mouseX = Camera.main.ScreenToWorldPoint (new Vector3(Input.mousePosition.x, 
             Input.mousePosition.y, -Camera.main.transform.position.z)).x;
         float playerMouseDistance = mouseX - transform.position.x;
         Vector2 prevMoveVec = moveVec;
         moveVec = playerMouseDistance < 0 ? Vector2.left : Vector2.right;
         
+        // 포인터 거리에 따라 이동 상태
         if (Mathf.Abs(playerMouseDistance) <= mouseXThreshold/10)
         {
             moveState = MoveState.Idle;
-            if (moveCo != null) StopCoroutine(moveCo);
+            if (moveCo != null) {
+                StopCoroutine(moveCo);
+                moveCo = null;
+            };
             return;
         }
         else if (Mathf.Abs(playerMouseDistance) <= mouseXThreshold)
@@ -51,34 +58,29 @@ public class Player : MonoBehaviour
             if (moveState == MoveState.Walk && moveVec == prevMoveVec)
                 return;
             moveState = MoveState.Walk;
-            if(moveCo!=null) StopCoroutine(moveCo);
-            moveCo = StartCoroutine(WalkCo(moveVec));
+            if (moveCo != null) 
+                StopCoroutine(moveCo); 
+            moveCo = StartCoroutine(WalkCo(moveVec * walkSpeed));
         }
         else if(Mathf.Abs(playerMouseDistance) > mouseXThreshold)
         {
             if (moveState == MoveState.Run && moveVec == prevMoveVec)
                 return;
             moveState = MoveState.Run;
-            if (moveCo != null) StopCoroutine(moveCo);
-            moveCo = StartCoroutine(RunCo(moveVec));
-        } 
+            if (moveCo != null) 
+                StopCoroutine(moveCo); 
+            moveCo = StartCoroutine(WalkCo(moveVec * runSpeed));
+        }
     }
     IEnumerator WalkCo(Vector2 vec)
     {
-        while(true)
-        {
-            transform.Translate(vec * walkSpeed * Time.deltaTime);
-            yield return null;
-        }
-    }
-    IEnumerator RunCo(Vector2 vec)
-    {
+        OnPlayerMoved?.Invoke(vec);
         while (true)
         {
-            transform.Translate(vec * runSpeed * Time.deltaTime);
+            transform.Translate(vec  * Time.deltaTime);
             yield return null;
         }
-    }
+    } 
     private void SendEyeLaser()
     {
 
